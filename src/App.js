414cd4d14f4427e73b0249f178147c69fd35f774
Paulo Gabriel <gabrieeel.c@gmail.com>
2022-05-11 14:16:31 -0300

    feito o layout da tela 1, tela de boas-vindas ao usuário.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+export default function App(){
+
+
+    return(
+        <>
+        <div className="container">
+            <div className="welcomeScreen">
+                <div><img src="./assets/image/logo.png"></img></div> 
+                <div><h1>ZapRecall</h1></div>
+                <div><button>Iniciar Recall!</button></div>   
+            </div>
+        </div>      
+        </>
+    );
+};
